fa693246fcc1ae8880ed083b49c6c3df8211cd65
Eric Aldrich <accordltn@gmail.com>
2015-07-09 01:50:49 -0700

    math_handler needs fixing, but all other exalted functionality is up.

--- lib/exalted.rb
+++ lib/exalted.rb
@@ -41,59 +41,94 @@ class Exalted
       math_array = math_handler(math_array)
     end
 
-    # Debugging
-    # m.reply rolls_handled.to_s
-    # m.reply response + cosmetic_array.join('') + " ---> " + math_array.join('')
-    # m.reply response + math_array.join('')
-
     # Finally time for exalted specific things
 
     # !exm disables double_tens
     if input[0] =~ /m/
       double_tens = false
     end
-
+    m.reply response + input[2]
     # !exs supplies a new target number, should be in input[2]
     if input[0] =~ /s/
       if input[2] =~ /^\d*$/
         target_number = input[2].to_i
       else
-        m.reply response + "Improper use of !exs, please provide a number as the second arguement."
+        m.reply response + "Improper use of !exs, please provide a target number as the second arguement."
         return 1
       end
     end
 
-    roll_array = roller(10, math_array[0])
+    # Were there any auto-successes/penalties?
+    if input[2] =~ /^\+\d*$/
+      auto_success = input[2].to_i
+    elsif input[3] =~ /^\+\d*$/
+      auto_success = input[2].to_i
+    elsif input[2] =~ /^-\d*$/
+      auto_success = -(input[2].to_i)
+    elsif input[3] =~ /^-\d*$/
+      auto_success = -(input[2].to_i)
+    end
 
+    # Performing Exalted rolls
+    roll_array = roller(10, math_array[0].to_i)
+
+    # Totalling successes
     successes = success_count(roll_array, double_tens, auto_success, target_number)
 
+    # Add total rolls to response
+    response += "(" + math_array[0].to_s + ") "
 
+    # Add ordered array to response
+    response += roll_array.sort.reverse.join(', ')
 
+    # Add successes to response
+    if successes > 0
+      response += "  Successes: " + successes.to_s
+    else
+      response += "  Botch."
+    end
 
-  #   rolls = []
+    # Add unordered array to response
+    response += "           " + roll_array.to_s
 
-		# # Need to parse +'s and -'s
-		# total_rolls = input[1].to_i
-
-		
-		# rolls = roller(10, total_rolls)
-		# ordered_rolls = rolls.sort.reverse
-
-		# # Add total rolls to response
-		# response += "(" + total_rolls.to_s + ") "
-		# # Add ordered rolls to response
-		# response += ordered_rolls.join(', ') 
-		# # Add unordered rolls to response
-		# response += "      " + rolls.to_s
-		
-		# # Send response
-		# m.reply response
+    # Send that response!
+    m.reply response
 	end
 
-  def success_count (roll_array, double_tens, auto_success = 0,target_number = 7)
-
+  def success_count (roll_array, double_tens = true, auto_success = 0,target_number = 7)
+    successes = auto_success
+    roll_array.each do |x|
+      if x.to_i == 10 && double_tens
+        successes += 2
+      elsif x.to_i >= target_number
+        successes += 1
+      end
+    end
+    return successes
   end
 
 end
 
 # !exam 14+5-2 +2 Someday this will work
+
+
+
+
+#   rolls = []
+
+    # # Need to parse +'s and -'s
+    # total_rolls = input[1].to_i
+
+    
+    # rolls = roller(10, total_rolls)
+    # ordered_rolls = rolls.sort.reverse
+
+    # # Add total rolls to response
+    # response += "(" + total_rolls.to_s + ") "
+    # # Add ordered rolls to response
+    # response += ordered_rolls.join(', ') 
+    # # Add unordered rolls to response
+    # response += "      " + rolls.to_s
+    
+    # # Send response
+    # m.reply response
