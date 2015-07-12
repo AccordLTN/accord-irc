3a26ba87b23febb9679db77b685c66651dd4de13
Eric Aldrich <accordltn@gmail.com>
2015-07-11 23:59:43 -0700

    Made smack_snark a bit less annoying.

--- lib/accord_snark.rb
+++ lib/accord_snark.rb
@@ -8,8 +8,6 @@ class AccordSnark
 
   match "botsmack", method: :smack_snark
   def smack_snark(m)
-    m.reply "World 4EC59M found incongruent with expected parameters.  Sanitizing."
-    m.reply "Here is the part where #{m.user.nick} would be kicked, but I don't know how to yet."
-    m.reply "Sanitized."
+    m.reply "I'll remember that."
   end
 end
